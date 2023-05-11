8d3db5de2cd6c574aa93b549c183699537a0b2cd
Manjeet Singh <manjeet6972@gmail.com>
2023-05-11 14:13:37 +0530

    Artistic Landing Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import { ThemeProvider } from 'styled-components';
+import ThemeContext from './contexts/ThemeContext';
+import LandingPage from './pages/LandingPage';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={{ theme }}>
+        <GlobalStyles />
+        <LandingPage />
+      </ThemeProvider>
+    </>
   );
 }
 
